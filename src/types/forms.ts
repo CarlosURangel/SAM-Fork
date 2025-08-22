152848d813500ff3197ae3d1370961f9531631dd
TomasNieto7 <141538649+TomasNieto7@users.noreply.github.com>
2025-08-22 14:40:47 -0600

    feat: added RegisterPrivateLesson and AddStudentDialog components

--- src/types/forms.ts
+++ src/types/forms.ts
@@ -1,13 +1,29 @@
 
-export interface TextInputProps {
+export interface TextInputProps<T extends string | number = string> {
   label: string;
+  value: T;
+  onChange: (value: T) => void;
+  placeholder?: string;
+  error?: string;
+  disabled?: boolean;
+  type?: string;
+  max?: number;
+  min?: number;
+  className?: string;
+}
+
+export interface PasswordInputProps extends TextInputProps {
+  toggleVisibility?: boolean;
+}
+
+export interface SelectFormProps {
+  label: string;
+  selectLabel: string;
+  options: string[];
   value: string;
   onChange: (value: string) => void;
   placeholder?: string;
   error?: string;
   disabled?: boolean;
-}
-
-export interface PasswordInputProps extends TextInputProps {
-  toggleVisibility?: boolean;
+  className?: string;
 }
